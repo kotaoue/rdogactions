aad08c18c760a4baab00bf69e4da239257a591a6
kota oue <kota.oue@yappli.co.jp>
2020-08-29 06:58:03 +0900

    Update main.go

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("%s", "Hello World")
+	fmt.Printf("%s\n", "Hello World")
+	fmt.Printf("%s\n", helloWorld(""))
+}
+
+func helloWorld(s string) string {
+	if s == "lower" {
+		return "hello world"
+	} else {
+		return "HELLO WORLD"
+	}
 }
